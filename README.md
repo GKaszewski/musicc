8f2d81e44910915c743fef72b11d8eaaa238db15
Gabriel Kaszewski <gabrielkaszewski@gmail.com>
2020-12-07 17:17:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Tailwind with Typescript template
+
+## Usage
+1. Clone this repository
+2. Run `yarn`
+3. Run `yarn start` to start the server!
+Enjoy!
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-red-300 h-screen">
+      <h1 className="text-center text-white text-5xl">Hello, tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
